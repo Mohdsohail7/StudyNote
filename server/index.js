cfc2e5de3b17efb26fe41c3b448e66cea2b8eea2
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-20 16:15:29 +0530

    backend index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://study-note-xi.vercel.app/",
         credentials:true,
     })
 )
