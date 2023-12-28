65d0646e852a35ff42325137285c78b264a86813
Mohdsohail7 <sohailmohd437@gmail.com>
2023-12-28 17:49:05 +0530

    Update text

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
   return (
     
     <Template
-      title="Join the millions learning to code with StudyNotion for free"
+      title="Join the millions learning to code with StudyNote for free"
       description1="Build skills for today, tomorrow, and beyond."
       description2="Education to future-proof your career."
       image={signupImg}
